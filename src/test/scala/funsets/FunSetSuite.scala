522601313738b8ddbb637ae54bb81ffd17a9b382
esteban marin <estebanmarin002@gmail.com>
2022-08-03 19:07:05 -0500

    more solved

--- src/test/scala/funsets/FunSetSuite.scala
+++ src/test/scala/funsets/FunSetSuite.scala
@@ -1,10 +1,11 @@
 package funsets
 
-/**
- * This class is a test suite for the methods in object FunSets.
- *
- * To run this test suite, start "sbt" then run the "test" command.
- */
+import scala.compiletime.ops.int
+
+/** This class is a test suite for the methods in object FunSets.
+  *
+  * To run this test suite, start "sbt" then run the "test" command.
+  */
 class FunSetSuite extends munit.FunSuite:
 
   import FunSets.*
@@ -13,50 +14,30 @@ class FunSetSuite extends munit.FunSuite:
     assert(contains(x => true, 100))
   }
 
-  /**
-   * When writing tests, one would often like to re-use certain values for multiple
-   * tests. For instance, we would like to create an Int-set and have multiple test
-   * about it.
-   *
-   * Instead of copy-pasting the code for creating the set into every test, we can
-   * store it in the test class using a val:
-   *
-   *   val s1 = singletonSet(1)
-   *
-   * However, what happens if the method "singletonSet" has a bug and crashes? Then
-   * the test methods are not even executed, because creating an instance of the
-   * test class fails!
-   *
-   * Therefore, we put the shared values into a separate trait (traits are like
-   * abstract classes), and create an instance inside each test method.
-   *
-   */
+  /** When writing tests, one would often like to re-use certain values for
+    * multiple tests. For instance, we would like to create an Int-set and have
+    * multiple test about it.
+    *
+    * Instead of copy-pasting the code for creating the set into every test, we
+    * can store it in the test class using a val:
+    *
+    * val s1 = singletonSet(1)
+    *
+    * However, what happens if the method "singletonSet" has a bug and crashes?
+    * Then the test methods are not even executed, because creating an instance
+    * of the test class fails!
+    *
+    * Therefore, we put the shared values into a separate trait (traits are like
+    * abstract classes), and create an instance inside each test method.
+    */
 
   trait TestSets:
-    val s1 = singletonSet(1)
+    val s1: FunSet = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
-
-  /**
-   * This test is currently disabled (by using @Ignore) because the method
-   * "singletonSet" is not yet implemented and the test would fail.
-   *
-   * Once you finish your implementation of "singletonSet", remove the
-   * .ignore annotation.
-   */
-  test("singleton set one contains one".ignore) {
-
-    /**
-     * We create a new instance of the "TestSets" trait, this gives us access
-     * to the values "s1" to "s3".
-     */
-    new TestSets:
-      /**
-       * The string argument of "assert" is a message that is printed in case
-       * the test fails. This helps identifying which assertion failed.
-       */
-      assert(contains(s1, 1), "Singleton")
-  }
+    val a: FunSet = x => x >= 3 && x <= 7
+    val b: FunSet = x => x >= 5 && x <= 7
+    val c: FunSet = x => x >= 8 && x <= 10
 
   test("union contains all elements of each set") {
     new TestSets:
@@ -66,7 +47,26 @@ class FunSetSuite extends munit.FunSuite:
       assert(!contains(s, 3), "Union 3")
   }
 
+  test("interset contains all elements of each set") {
+    new TestSets:
+      val s = intersect(a, b)
+      assert(!contains(s, 3), "Union 1")
+      assert(contains(s, 6), "Union 2")
+  }
 
+  test("diff") {
+    new TestSets:
+      val s = diff(a, b)
+      assert(contains(s, 3), "Union 1")
+      assert(!contains(s, 6), "Union 2")
+  }
+  
+  test("filter") {
+    new TestSets:
+      val s = filter(b, a)
+      assert(!contains(s, 3), "Union 1")
+      assert(contains(s, 6), "Union 2")
+  }
 
   import scala.concurrent.duration.*
   override val munitTimeout = 10.seconds
